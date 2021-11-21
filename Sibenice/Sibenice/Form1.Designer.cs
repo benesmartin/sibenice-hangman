25b230d638cbde12d7b71214a7619d3817df1030
benesmartin <93920137+benesmartin@users.noreply.github.com>
2021-11-21 19:41:36 +0100

    initial commit
    basic full game

---
+++ Sibenice/Sibenice/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace Sibenice
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.tbChar = new System.Windows.Forms.TextBox();
+            this.lbWord = new System.Windows.Forms.Label();
+            this.lbChars = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb1
+            // 
+            this.pb1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.pb1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pb1.Image = global::Sibenice.Properties.Resources.hangman11;
+            this.pb1.InitialImage = null;
+            this.pb1.Location = new System.Drawing.Point(1, 0);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(405, 450);
+            this.pb1.TabIndex = 0;
+            this.pb1.TabStop = false;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(412, 387);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(387, 63);
+            this.btnNew.TabIndex = 2;
+            this.btnNew.Text = "Nové slovo";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // tbChar
+            // 
+            this.tbChar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tbChar.Location = new System.Drawing.Point(412, 229);
+            this.tbChar.MaxLength = 1;
+            this.tbChar.Name = "tbChar";
+            this.tbChar.Size = new System.Drawing.Size(387, 23);
+            this.tbChar.TabIndex = 3;
+            this.tbChar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbChar.TextChanged += new System.EventHandler(this.tbChar_TextChanged);
+            // 
+            // lbWord
+            // 
+            this.lbWord.BackColor = System.Drawing.SystemColors.Control;
+            this.lbWord.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbWord.Cursor = System.Windows.Forms.Cursors.No;
+            this.lbWord.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbWord.Location = new System.Drawing.Point(412, 0);
+            this.lbWord.Name = "lbWord";
+            this.lbWord.Size = new System.Drawing.Size(387, 226);
+            this.lbWord.TabIndex = 5;
+            this.lbWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbChars
+            // 
+            this.lbChars.BackColor = System.Drawing.SystemColors.Control;
+            this.lbChars.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbChars.Cursor = System.Windows.Forms.Cursors.No;
+            this.lbChars.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbChars.Location = new System.Drawing.Point(412, 255);
+            this.lbChars.Name = "lbChars";
+            this.lbChars.Size = new System.Drawing.Size(387, 129);
+            this.lbChars.TabIndex = 6;
+            this.lbChars.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbChars);
+            this.Controls.Add(this.lbWord);
+            this.Controls.Add(this.tbChar);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.pb1);
+            this.Name = "Form1";
+            this.Text = "Teacher Hangman";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.TextBox tbChar;
+        private System.Windows.Forms.Label lbWord;
+        private System.Windows.Forms.Label lbChars;
+    }
+}
+
---
+++ Sibenice/Sibenice/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sibenice
+{
+    public partial class Form1 : Form
+    {
+        private string[] slova = { "Kubín", "Haberzettl", "Douda", "Balín", "Medřický", "Horáček", "Neustupa", "Studihradová", "Beneš", "Brychtová", "Hoffmann", "Kučera", "Kříž", "Rys" };
+        private string[] diakritika = { "Á", "É", "Ě", "Í", "Ý", "Ó", "Ú", "Ů", "Ž", "Š", "Č", "Ř", "Ď", "Ť", "Ň" };
+        private static Random rand = new Random((int)DateTime.Now.Ticks);
+        private List<string> spravne = new List<string>();
+        private List<string> spatne = new List<string>();
+        private string nahodneSlovo, nahodneSlovoUppercase, pismeno;
+        private int pokusy = 10;
+        private bool vyhral = false, prohral = false;
+        private StringBuilder sibeniceSlovo;
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+        static string RemoveAccents(string input)
+        {
+            string normalized = input.Normalize(NormalizationForm.FormKD);
+            Encoding removal = Encoding.GetEncoding(Encoding.ASCII.CodePage, new EncoderReplacementFallback(""), new DecoderReplacementFallback(""));
+            byte[] bytes = removal.GetBytes(normalized);
+            return Encoding.ASCII.GetString(bytes);
+        }
+        private void newWord()
+        {
+            pokusy = 10;
+            lbChars.Text = "✅ \n❎ \nPokusy: " + Convert.ToString(pokusy);
+            spravne.Clear();
+            spatne.Clear();
+            vyhral = false;
+            prohral = false;
+            pb1.Image = Properties.Resources.hangman11;
+            nahodneSlovo = slova[rand.Next(0, slova.Length)];
+            nahodneSlovoUppercase = RemoveAccents(nahodneSlovo.ToUpper());
+            sibeniceSlovo = new StringBuilder(nahodneSlovo.Length);
+            for (int i = 0; i < nahodneSlovo.Length; i++)
+            {
+                sibeniceSlovo.Append('-');
+            }
+        }
+        private void Game()
+        {
+            if (vyhral == false && pokusy >= 0)
+            {
+                pismeno = tbChar.Text.ToUpper();
+                if (spravne.Contains(pismeno))
+                {
+                    spravne.Remove(pismeno);
+                }
+                if (spatne.Contains(pismeno))
+                {
+                    spatne.Remove(pismeno);
+                    pokusy++;
+                }
+                if (nahodneSlovoUppercase.Contains(pismeno))
+                {
+                    spravne.Add(pismeno);
+                    for (int i = 0; i < nahodneSlovo.Length; i++)
+                    {
+                        if (Convert.ToString(nahodneSlovoUppercase[i]) == pismeno)
+                        {
+                            sibeniceSlovo[i] = nahodneSlovo[i];
+                        }
+                    }
+                    if (Convert.ToString(sibeniceSlovo) == nahodneSlovo)
+                    {
+                        vyhral = true;
+                    }
+                }
+                else
+                {
+                    spatne.Add(pismeno);
+                    pokusy--;
+                    if (pokusy == 9)
+                    {
+                        pb1.Image = Properties.Resources.hangman10;
+                    }
+                    if (pokusy == 8)
+                    {
+                        pb1.Image = Properties.Resources.hangman9;
+                    }
+                    if (pokusy == 7)
+                    {
+                        pb1.Image = Properties.Resources.hangman8;
+                    }
+                    if (pokusy == 6)
+                    {
+                        pb1.Image = Properties.Resources.hangman7;
+                    }
+                    if (pokusy == 5)
+                    {
+                        pb1.Image = Properties.Resources.hangman6;
+                    }
+                    if (pokusy == 4)
+                    {
+                        pb1.Image = Properties.Resources.hangman5;
+                    }
+                    if (pokusy == 3)
+                    {
+                        pb1.Image = Properties.Resources.hangman4;
+                    }
+                    if (pokusy == 2)
+                    {
+                        pb1.Image = Properties.Resources.hangman3;
+                    }
+                    if (pokusy == 1)
+                    {
+                        pb1.Image = Properties.Resources.hangman2;
+                    }
+                    if (pokusy == 0)
+                    {
+                        pb1.Image = Properties.Resources.hangman;
+                        prohral = true;
+                    }
+                }
+                lbWord.Text = sibeniceSlovo.ToString();
+            }
+            if (vyhral)
+            {
+                if (pokusy == 10)
+                {
+                    MessageBox.Show("Perfekt!\nDokázal jsi vyhrát bez ztráty pokusu!", "Gratuluji!");
+                }
+                else
+                {
+                    MessageBox.Show("Vyhrál jsi!", "Gratuluji!");
+                }
+
+            }
+            if (prohral)
+            {
+                MessageBox.Show("Prohrál jsi!\nSprávná odpověď je: " + nahodneSlovo, "Bohužel!");
+            }
+            lbChars.Text = "✅ " + string.Join(',', spravne) + "\n❎ " + string.Join(',', spatne) + "\nPokusy: " + Convert.ToString(pokusy);
+        }
+        private void configControls()
+        {
+            this.ActiveControl = tbChar;
+            this.tbChar.AutoSize = false;
+
+            pb1.Location = new Point(0, 0);
+            pb1.Size = new Size(ClientRectangle.Width / 2, ClientRectangle.Height);
+            pb1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            lbWord.Size = new Size(pb1.Width, Convert.ToInt32(pb1.Height * 0.3));
+            lbWord.Location = new Point(pb1.Width, 0);
+            lbWord.Font = new Font("Arial", this.ClientSize.Height / 15);
+            lbWord.Text = sibeniceSlovo.ToString();
+
+            btnNew.Size = new Size(pb1.Width, Convert.ToInt32(pb1.Height * 0.125));
+            btnNew.Location = new Point(ClientRectangle.Width - btnNew.Width, ClientRectangle.Height - btnNew.Height);
+            btnNew.Font = new Font("Arial", this.ClientSize.Height / 15);
+
+            tbChar.Size = new Size(pb1.Width, Convert.ToInt32(pb1.Height * 0.2));
+            tbChar.Location = new Point(ClientRectangle.Width - tbChar.Width, lbWord.Location.Y + lbWord.Height);
+            tbChar.Font = new Font("Arial", Convert.ToInt32(tbChar.Height * 0.75));
+
+            lbChars.Size = new Size(pb1.Width, Convert.ToInt32(pb1.Height * 0.375));
+            lbChars.Location = new Point(ClientRectangle.Width - lbChars.Width, tbChar.Location.Y + tbChar.Height);
+            lbChars.Font = new Font("Arial", this.ClientSize.Height / 20);
+            lbChars.Text = "✅ " + string.Join(',', spravne) + "\n❎ " + string.Join(',', spatne) + "\nPokusy: " + Convert.ToString(pokusy);
+        }
+        public void Init()
+        {
+            newWord();
+            configControls();
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            configControls();
+        }
+        private void tbChar_TextChanged(object sender, EventArgs e)
+        {
+            if (tbChar.Text != "")
+            {
+                if (tbChar.Text != RemoveAccents(tbChar.Text))
+                {
+                    for (int i = 0; i < diakritika.Length; i++)
+                    {
+                        if (tbChar.Text.ToUpper() == diakritika[i])
+                        {
+                            tbChar.Text = RemoveAccents(tbChar.Text);
+                        }
+                    }
+                }
+                else
+                {
+                    Game();
+                    tbChar.Text = "";
+                }
+            }
+        }
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            newWord();
+            lbWord.Text = sibeniceSlovo.ToString();
+            this.ActiveControl = tbChar;
+        }
+    }
+}
